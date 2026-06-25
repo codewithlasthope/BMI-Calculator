8a57d08a11cb92e211586fcd3fdfda1493234dee
codewithlasthope <codewithlasthope@gmail.com>
2026-06-25 16:12:38 +0600

    added dark mode

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 let weight = document.getElementById("kg");
 let height = document.getElementById("cm");
 let result = document.getElementById("result");
+let toggleBtn = document.getElementById('themeToggle')
+
+// Load saved theme
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+    toggleBtn.textContent = '☀️ Light Mode';
+    toggleBtn.classList.remove('light');
+    toggleBtn.classList.add('dark');
+}
+
+// Toggle theme
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    const isDark = document.body.classList.contains('dark-mode');
+
+    if (isDark) {
+        toggleBtn.textContent = '☀️ Light Mode';
+        toggleBtn.classList.remove('light');
+        toggleBtn.classList.add('dark');
+
+        localStorage.setItem('theme', 'dark');
+    } else {
+        toggleBtn.textContent = '🌙 Dark Mode';
+        toggleBtn.classList.remove('dark');
+        toggleBtn.classList.add('light');
+
+        localStorage.setItem('theme', 'light');
+    }
+});
+
 function calculateBMI() {
     let w = weight.value
-    let h = height.value/100
+    let h = height.value / 100
     let bmi = w / (h * h)
     result.innerText = "Your BMI is " + bmi.toFixed(2);
     if (bmi < 18.5) {
